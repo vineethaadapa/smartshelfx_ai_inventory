57dbfb775d6bf2b6dcced5e78d85f703f77931f3
vineethaadapa <vineetha.adapa@sasi.ac.in>
2026-05-18 21:51:00 +0530

    cors fixed

--- smartshelfx-backend/src/main/java/com/smartshelfx/smartshelfx_backend/Config/SecurityConfig.java
+++ smartshelfx-backend/src/main/java/com/smartshelfx/smartshelfx_backend/Config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://smartshelfxdb.netlify.app/login")); 
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://smartshelfxdb.netlify.app/")); 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
